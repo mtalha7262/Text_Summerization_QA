e183ad24451c844c7da40f369e1ddedc55652c24
Muhammad Tallha <115509838+mtalha7262@users.noreply.github.com>
2025-09-16 14:41:52 +0500

    ok

---
+++ main.py
@@ -0,0 +1,5 @@
+from transformers import pipeline
+from langchain_huggingface import HuggingFacePipeline
+from langchain.prompts import PromptTemplate
+from transformers.utils.logging import set_verbosity_error
+set_verbosity_error()
