77f4ddcb7ef7ddecf30abffb818ecee82625ac18
Muhammad Tallha <115509838+mtalha7262@users.noreply.github.com>
2025-09-16 14:44:12 +0500

    ok

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ from langchain_huggingface import HuggingFacePipeline
 from langchain.prompts import PromptTemplate
 from transformers.utils.logging import set_verbosity_error
 set_verbosity_error()
+
+summarization_pipeline = pipeline("summarization", model="facebook/bart-large-cnn")
+summerizer = HuggingFacePipeline(pipeline=summarization_pipeline)
+
+refinement_pipeline = pipeline(task="summarization", model="facebook/bart-large")
+refiner = HuggingFacePipeline(pipeline=refinement_pipeline)
+
+qa_pipeline = pipeline(task="question-answering", model="deepset/roberta-base-squad2")
+qa = HuggingFacePipeline(pipeline=qa_pipeline)
